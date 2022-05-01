7b2bab352a0c650069b0f00877d745bbe3be78f0
Sreedath Somi Setty <82104483+SreedathSomiSetty@users.noreply.github.com>
2022-05-01 13:13:36 +0530

    Add files via upload

---
+++ adminheader.php
@@ -0,0 +1,58 @@
+
+<html>
+    <head>
+        <title>admin</title>
+        <link href="bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">Admin</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+
+      <li class="nav-item">
+        <a class="nav-link text-white" href="db.php?home">Home</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link text-white" href="db.php?profile">My Profile</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link text-white" href="db.php?logout">Logout</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link text-white" href="db.php?vu">Verified users</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link text-white" href="db.php?nvu">Not Verified users</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link text-white" href="db.php?nvm">Not Verified Members</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link text-white" href="db.php?vm">Verified Members</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link text-white" href="postblog.php">Post Blog</a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link text-white" href="db.php?vb">Verified blogs</a>
+      </li>
+
+      <li class="nav-item">
+        <a class="nav-link text-white" href="db.php?nvb">Not Verified blogs</a>
+      </li>
+
+    </ul>
+  </div>
+</nav>
